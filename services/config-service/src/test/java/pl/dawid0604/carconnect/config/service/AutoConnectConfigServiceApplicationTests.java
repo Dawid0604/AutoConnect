8cec2c441d31dc6396d30ac2846c6f7995ed1096
Dawid Sikora <tadek123tadek@o2.pl>
2025-06-06 23:01:36 +0200

    feat: Config Service - basic implementation

---
+++ services/config-service/src/test/java/pl/dawid0604/carconnect/config/service/AutoConnectConfigServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.dawid0604.carconnect.config.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoConnectConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		AutoConnectConfigServiceApplication.main(new String[] { });
+	}
+
+}
