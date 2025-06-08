7cefc928e33418a41d91cb0a2a6549ff9747f1fe
Dawid Sikora <93526656+Dawid0604@users.noreply.github.com>
2025-06-08 22:43:46 +0200

    feat: Notification Service - basic implementation (#12)
    feat: Notification service - basic implementation
    feat: adding dashboards.yml

---
+++ services/notification-service/src/main/java/pl/dawid0604/autoconnect/notification/service/AutoConnectNotificationServiceApplication.java
@@ -0,0 +1,17 @@
+package pl.dawid0604.autoconnect.notification.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableCaching
+@SpringBootApplication
+@EnableDiscoveryClient
+class AutoConnectNotificationServiceApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(AutoConnectNotificationServiceApplication.class, args);
+	}
+
+}
