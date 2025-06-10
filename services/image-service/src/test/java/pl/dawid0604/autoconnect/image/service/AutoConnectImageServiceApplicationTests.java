246cef62a1a29aebee6abce768092002ab99b6f2
Dawid Sikora <tadek123tadek@o2.pl>
2025-06-10 21:19:02 +0200

    feat: Image Service - basic implementation

---
+++ services/image-service/src/test/java/pl/dawid0604/autoconnect/image/service/AutoConnectImageServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.dawid0604.autoconnect.image.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoConnectImageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		AutoConnectImageServiceApplication.main(new String[]{ });
+	}
+
+}
