5a157ac49d5d047c4695aace8d26d7e5a11163ca
Dawid0604 <tadek123tadek@o2.pl>
2025-07-14 20:34:55 +0200

    refactor: making Config Service multimodule

--- services/config-service/application/src/main/java/pl/dawid0604/autoconnect/config/service/AutoConnectConfigServiceApplication.java
+++ services/config-service/application/src/main/java/pl/dawid0604/autoconnect/config/service/AutoConnectConfigServiceApplication.java
@@ -2,13 +2,11 @@ package pl.dawid0604.autoconnect.config.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Application runtime.
  */
-@EnableCaching
 @EnableConfigServer
 @SpringBootApplication
 final class AutoConnectConfigServiceApplication {
--- services/config-service/application/src/test/java/pl/dawid0604/autoconnect/config/service/AutoConnectConfigServiceApplicationTests.java
+++ services/config-service/application/src/test/java/pl/dawid0604/autoconnect/config/service/AutoConnectConfigServiceApplicationTests.java
@@ -1,13 +1,16 @@
 package pl.dawid0604.autoconnect.config.service;
 
+import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
+import static lombok.AccessLevel.PACKAGE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@NoArgsConstructor(access = PACKAGE)
 class AutoConnectConfigServiceApplicationTests {
 
 	@Autowired
